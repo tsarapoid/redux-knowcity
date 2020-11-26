2b354ec057bd3e66cd716850f540683e3a5b6e84
tsarapoid <nadein@gmail.com>
2020-11-26 16:59:41 +0300

    compose refactor

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
 
-import App from './App';
+import App from './App'
 import rootReducer from './RootReducer'
-import './Styles/index.css';
+import './Styles/index.css'
 
-
+/* 
 const store = createStore(
-	rootReducer,
-	applyMiddleware(logger))
+rootReducer,
+applyMiddleware(logger));
+*/
+
 /* 
 window.__REDUX_DEVTOOLS_EXTENSION__ &&
 window.__REDUX_DEVTOOLS_EXTENSION__())
- */
+*/
+ 
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(logger),
+//  applyMiddleware(thunk),
+    reduxDevtools && reduxDevtools()
+  )
+)
+
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
